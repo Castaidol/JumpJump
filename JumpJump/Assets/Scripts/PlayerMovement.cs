883b5f15edb27ff4c2beff6f28611df81a61d256
Castaidol <gabriele.castaway@gmail.com>
2018-03-13 12:40:16 +0800

    Basic player movement
    Veeeery basic

--- JumpJump/Assets/Scripts/PlayerMovement.cs
+++ JumpJump/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float jumpForce = 5f;
+
+    private Transform playerTransform;
+    private Rigidbody2D rb2d;
+
+	private void Awake()
+	{
+        playerTransform = transform;
+        rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	public void MovePlayer(){
+
+        rb2d.AddForce(new Vector2(1, jumpForce), ForceMode2D.Impulse);
+
+
+    }
+
 }
