034b351ec3cc55b308a53d19832ee2c99eb57b2e
Castaidol <gabriele.castaway@gmail.com>
2018-03-15 15:42:59 +0800

    Destroy tiles
    destroy the tiles used

---
+++ JumpJump/Assets/Scripts/DestroyTile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTile : MonoBehaviour {
+
+    public int offsetx = 3;
+
+    private float leftEdgeDestroy;
+    private Camera cam;
+    private Transform myTransform;
+
+	private void Awake()
+	{
+        cam = Camera.main;
+        myTransform = transform;
+        BoxCollider2D tileBoxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	
+	void Update () {
+        DestroyThisTile();
+	}
+
+    void DestroyThisTile()
+    {
+        float cameraHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+        leftEdgeDestroy = cam.transform.position.x - cameraHorizontalExtend - offsetx;
+
+        if (myTransform.position.x <= leftEdgeDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
